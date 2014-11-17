44f4f63d67869a21fb74020f901a2fc82504748a
Christopher Potts <cgpotts@gmail.com>
2014-11-16 18:33:21 -0800

    Code overall for true *lexical* uncertainty

--- code/compositional_uncertainty.py
+++ code/compositional_uncertainty.py
@@ -6,6 +6,7 @@ import numpy as np
 from fragment import *
 from pragmods import display_matrix, Pragmod
 from semgrammar import get_best_inferences
+import matplotlib.pyplot as plt
 
 NULL = 'NULL'
 
@@ -13,14 +14,15 @@ class UncertaintyLexicon:
     def __init__(self,
                  entities=entities,
                  shot_entities=shot_entities,
+                 basic_states=(0,1,2),
                  subj_dets=('every', 'some', 'exactly_one', 'no'),
                  obj_dets=('every', 'some', 'no'),
                  enrichable_dets=('some','PlayerA', 'PlayerB', 'PlayerC'),
                  proper_names=('PlayerA', 'PlayerB', 'PlayerC')):
-
         self.entities = entities
         self.shot_entities = shot_entities
         self.domain = self.entities + self.shot_entities
+        self.basic_states = basic_states
         self.subj_dets = subj_dets
         self.obj_dets = obj_dets
         self.dets = self.subj_dets + self.obj_dets
@@ -32,6 +34,7 @@ class UncertaintyLexicon:
         self.worlds = []
         self.messages = []
         self.matrices = []
+        self.langs = []
 
     def double_quantifier_language(self):
         self.get_worlds()
@@ -77,18 +80,20 @@ class UncertaintyLexicon:
         for lex in self.lexica:
             lang = defaultdict(list)
             lang[NULL] = self.worlds
-            for det1, det2 in itertools.product(self.subj_dets, self.obj_dets):
-                subj_str = "%s(player)" % det1
-                subj = lex[subj_str]
-                obj_str = "%s(shot)" % det2
-                obj = lex[obj_str]
-                msg = "%s(made(%s))" % (subj_str, obj_str)
-                self.messages.append(msg)
-                for world in self.worlds:
-                    made = intensional_made(self.entities, self.shot_entities, world)
+            for world in self.worlds:
+                made = intensional_made(self.entities, self.shot_entities, world)
+                for det1, det2 in itertools.product(self.subj_dets+self.proper_names, self.obj_dets):
+                    # No complement need be added for proper names:
+                    if det1 in self.subj_dets:                        
+                        det1 = "%s(player)" % det1                    
+                    subj = lex[det1]
+                    obj_str = "%s(shot)" % det2
+                    obj = lex[obj_str]
+                    msg = "%s(made(%s))" % (det1, obj_str)
+                    self.messages.append(msg)            
                     val = (lambda Y : Y in subj)(made((lambda X : X in obj)))
                     if val:
-                        lang[msg].append(world)                
+                        lang[msg].append(world)            
             langs.append(lang)
         self.messages = sorted(set(self.messages))
         self.messages.append(NULL)                               
@@ -102,13 +107,11 @@ class UncertaintyLexicon:
             for world in self.worlds:
                 scored = [x for x in domain if x in intensional_made(self.entities, self.shot_entities, world)(some(self.shot_entities))]                
                 for det in self.subj_dets + self.proper_names:
-                    subj_str = None
+                    # No complement need be added for proper names:                  
                     if det in self.subj_dets:
-                        subj_str = "%s(player)" % det                     
-                    else:
-                        subj_str = det
-                    msg = "%s(scored)" % subj_str
-                    subj = lex[subj_str]                    
+                        det = "%s(player)" % det                     
+                    msg = "%s(scored)" % det
+                    subj = lex[det]                    
                     self.messages.append(msg)
                     val = (lambda Y : Y in subj)(scored)
                     if val:
@@ -119,7 +122,7 @@ class UncertaintyLexicon:
         return langs
 
     def get_worlds(self, increasing=True):
-        self.worlds = list(itertools.product((0,1,2), repeat=len(self.entities)))
+        self.worlds = list(itertools.product(self.basic_states, repeat=len(self.entities)))
         if increasing:
             self.worlds = [w for w in self.worlds if self.check_increasing(w)]
 
@@ -147,7 +150,8 @@ class UncertaintyLexicon:
                         null_cost=5.0,
                         prior=None,
                         lexprior=None,
-                        temperature=1.0):
+                        temperature=1.0,
+                        n=1):
         if prior == None:
             prior = np.repeat(1.0/len(self.worlds), len(self.worlds))
         if lexprior == None:
@@ -160,45 +164,168 @@ class UncertaintyLexicon:
                       costs=costs,
                       prior=prior,
                       lexprior=lexprior,
-                      temperature=0.5)
+                      temperature=1.0)        
+        self.langs = mod.run_uncertainty_model(n=n, display=False)
+        # Smith et al. anxiety listener:
+        # langs = mod.run_anxiety_model(n=1, display=False)
+        # Anxiety/uncertainty:
+        # langs = mod.run_expertise_model(n=3, display=False)
+        # langs[-1] = np.sum(langs[-1], axis=0)
 
-        langs = mod.run_uncertainty_model(n=1, display=False)
-        best_inferences = get_best_inferences(langs[-1], self.messages, self.worlds)
-        if display:
-            alph = 'NSA'
-            display_matrix(langs[-1], rnames=self.messages, cnames=["".join([alph[i] for i in w]) for w in self.worlds])
-            for key, val in sorted(best_inferences.items()):
-                print key, val 
-        return langs[-1], best_inferences
+    def report(self, digits=4):
+        print 'Lexical', len(self.matrices)
+        print 'Final listener'
+        display_matrix(self.langs[-1], rnames=self.messages, cnames=self.get_worldnames(), digits=2)
+        print 'Best inferences'
+        best_inferences = self.get_best_inferences(digits=4)  
+        for msg, val in sorted(best_inferences.items()):
+            print msg, val
 
+    def get_best_inferences(self, digits=4):    
+        best_inferences = {}
+        # Round to avoid tiny distinctions that don't even display:
+        final_listener = np.round(self.langs[-1], 10)
+        for i, msg in enumerate(self.messages):
+            maxval = max(final_listener[i])
+            best_states = [(self.get_worldname(w), str(np.round(maxval, digits))) for j, w in enumerate(self.worlds) if final_listener[i,j] == maxval]
+            best_inferences[msg] = best_states             
+        return best_inferences   
 
+    def get_worldname(self, w):
+        alph = 'NSA'
+        return "".join([alph[i] for i in w])
+
+    def get_worldnames(self):
+        return [self.get_worldname(w) for w in self.worlds]
+    
+    def final_listener2latex(self, digits=2):        
+        mat = self.langs[-1]
+        rnames = self.messages
+        cnames = self.get_worldnames()
+        mat = np.round(mat, digits)
+        rows = []
+        rows.append([''] + cnames)
+        for i in range(mat.shape[0]):
+            rowmax = max(mat[i])
+            def highlighter(x): return r"\graycell{%s}" % x if x == rowmax else str(x)
+            vals = [highlighter(x) for x in mat[i]]            
+            rows.append([rnames[i]] + vals)
+        s = ""
+        s += "\\begin{tabular}[c]{r *{%s}{r} }\n" % len(cnames)
+        s += "\\toprule\n"
+        s += "%s\\\\\n" % " & ".join(rows[0])
+        s += "\\midrule\n"
+        for row in rows[1: ]:
+            s += "%s\\\\\n" % " & ".join(row)
+        s += "\\bottomrule\n"
+        s += "\\end{tabular}"
+        return s
+   
+    def final_listener2plot(self, output_filename=None, nrows=None, ncols=None, include_null=True, indices=None):
+        mat = self.langs[-1]
+        rnames = self.messages
+        cnames = self.get_worldnames()
+        if not nrows:       
+            nrows = len(self.subj_dets + self.proper_names)
+            ncols = len(self.obj_dets)
+        plt.style.use('ggplot')
+        fig, axarray = plt.subplots(nrows=nrows, ncols=ncols)
+        fig.set_figheight(3*nrows)
+        fig.set_figwidth(7*ncols)
+        fig.subplots_adjust(bottom=-0.5)
+        if indices == None:
+            indices = list(itertools.product(range(nrows), range(ncols)))        
+        if not include_null:
+            mat = mat[:-1]
+        for i, row in enumerate(mat):
+            axind = indices[i]
+            rowinitial = False
+            if axind[1] == 0:
+                rowinitial = True        
+            self.dist2plot(axarray[axind], row, title=rnames[i], cnames=cnames, rowinitial=rowinitial)
+        if output_filename:
+            plt.savefig(output_filename, bbox_inches='tight')
+        else:
+            plt.show()
+
+    def dist2plot(self, ax, row, title="", cnames=None, rowinitial=True):
+        width = 1.0
+        pos = np.arange(0, len(row), width)
+        ax.bar(pos, row, width)
+        ax.set_xlim([0.0, len(row)])
+        ax.set_xticks(pos+(width/2.0))
+        ax.set_xticklabels(cnames, fontsize=14,  rotation='vertical', color='black')
+        ax.set_ylim([0.0, 1.0])
+        ylabel = r"$L(w \mid m)$"        
+        if not rowinitial:
+            ylabel = ""
+            ax.set_yticklabels("")   
+        ax.set_ylabel(ylabel, fontsize=14)         
+        ax.set_title(title)        
+            
 ######################################################################
 
 if __name__ == '__main__':
 
     ##################################################
     # Illustrative example
-    
-    experiment = UncertaintyLexicon(entities=[a,b],
-                                    shot_entities=shot_entities,
-                                    subj_dets=('every', 'some', 'exactly_one', 'no'),
-                                    obj_dets=(),
-                                    enrichable_dets=('some', 'PlayerA', 'PlayerB'),
-                                    proper_names=('PlayerA', 'PlayerB'))
-    experiment.subject_scoring_language()
-    experiment.uncertainty_run()
 
+    def simple_example():
+        experiment = UncertaintyLexicon(entities=[a,b],
+                                        basic_states=(0,1),
+                                        shot_entities=shot_entities,
+                                        subj_dets=('every', 'some', 'no'),
+                                        obj_dets=(),
+                                        enrichable_dets=('some', 'PlayerA', 'PlayerB'),
+                                        proper_names=('PlayerA', 'PlayerB'))
+        experiment.subject_scoring_language()
+        experiment.uncertainty_run(temperature=1.0, n=1)
+        experiment.report()
+        print experiment.final_listener2latex(digits=2)
+        experiment.final_listener2plot(output_filename="../fig/example-simple.pdf", nrows=2, ncols=3, include_null=True)
+        
+    
     ##################################################
     # Modeling the pilot data
 
-    experiment = UncertaintyLexicon(entities=[a,b,c],
-                                    shot_entities=shot_entities,
-                                    subj_dets=('every', 'some', 'exactly_one', 'no'),
-                                    obj_dets=('every', 'some', 'no'),
-                                    enrichable_dets=('some',),
-                                    proper_names=())
-    experiment.double_quantifier_language()
-    experiment.uncertainty_run()
+    def pilot():
+        pilot = UncertaintyLexicon(entities=[a,b,c],
+                                   shot_entities=shot_entities,
+                                   subj_dets=('every', 'exactly_one', 'no'),
+                                   obj_dets=('every', 'some', 'no'),
+                                   enrichable_dets=('some',),
+                                   proper_names=())
+        pilot.double_quantifier_language()
 
-    
+        mcfrank_ordering = [(2, 2), (2, 0), (2, 1),
+                            (1, 2), (1, 0), (1, 1),
+                            (0, 2), (0, 0), (0, 1)]
+                            #(3, 2), (3, 0), (3, 1)] # if some is included put it first in subj_dets and use this line
+                        
+        pilot.uncertainty_run(temperature=1.0, n=1)
+        pilot.report()
+        print pilot.final_listener2latex(digits=2)
+        pilot.final_listener2plot(output_filename="../fig/example-pilot.pdf", include_null=False, indices=mcfrank_ordering)
+       
+
+    ##################################################
+    # Put it all together:
+
+    def full():
+        experiment = UncertaintyLexicon(entities=[a,b,c],
+                                        basic_states=(0,1,2),
+                                        shot_entities=shot_entities,
+                                        subj_dets=('every', 'some', 'no', 'exactly_one'),
+                                        obj_dets=('every', 'some', 'no', 'exactly_one'),
+                                        enrichable_dets=('some', 'PlayerA', 'PlayerB'), # Left out C because of memory issues.
+                                        proper_names=('PlayerA', 'PlayerB', 'PlayerC'))
+        experiment.double_quantifier_language()
+        experiment.uncertainty_run(temperature=1.0, n=1)
+        experiment.report()
+        print experiment.final_listener2latex(digits=2)
+        experiment.final_listener2plot(output_filename="../fig/example-full.pdf", include_null=False)
         
+
+    simple_example()
+    #pilot()
+    #full()
