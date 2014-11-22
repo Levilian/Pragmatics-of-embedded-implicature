79a2f04deb59a8957666630ee787b8f63acf8f2c
Christopher Potts <cgpotts@gmail.com>
2014-11-22 00:44:37 -0800

    Lexica simplified thanks to set-based fragment

--- code/grammar.py
+++ code/grammar.py
@@ -1,71 +1,68 @@
 #!/usr/bin/env python
 
+import sys
 import itertools
-from copy import copy
 import numpy as np
 from fragment import *
-from utils import display_matrix, NULL, powerset
+from utils import display_matrix, powerset, NULL
 
 ######################################################################
 
 class UncertaintyGrammars:
     def __init__(self,
-                 baselexicon=BASELEXICON,
+                 baselexicon=None,
                  messages=[],
                  worlds=[],
                  refinable={},
                  nullmsg=True):
-        
-        self.baselexicon = baselexicon
-        self.messages = messages
+
         self.worlds = worlds
         self.refinable = refinable
-        self.nullmsg = NULL
+        self.nullmsg = nullmsg
         if self.nullmsg:
-            self.messages.append(NULL)
-
+            messages.append(("NULL", None))
+        self.messages, self.formulae = zip(*messages)
+        self.messages = list(self.messages)
+        self.formulae = list(self.formulae)
+        self.baselexicon = baselexicon                       
+        
     def lexicon_iterator(self):
-        words_with_types, refinements = zip(*self.get_all_refinements().items())
-        print [len(x) for x in refinements]
-        m = len(self.messages)
-        n = len(self.worlds)        
+        words, refinements = zip(*self.get_all_refinements().items())               
         for meaning_vector in itertools.product(*refinements):
-            lex = dict(zip(words_with_types, meaning_vector))
-            mat = np.zeros((m, n))   
-            for i, msg in enumerate(self.messages):
-                mat[i] = self.worlds.interpret(msg, vectorize=True, withlex=lex)
-            # Final row for universally true NULL message:
-            if self.nullmsg:
-                mat[-1] = np.ones(n)
+            lex = dict(zip(words, meaning_vector))
+            mat = self.interpretation_matrix(lex)
+            # Lexica containing messages that denote {} need to be filtered on
+            # the current formulation of the model:
             if 0.0 not in np.sum(mat, axis=1):
-                yield mat 
+                yield mat
+
+    def interpretation_matrix(self, lexicon):
+        for word, sem in lexicon.items():
+            setattr(sys.modules[__name__], word, sem)
+        m = len(self.messages)
+        n = len(self.worlds)           
+        mat = np.zeros((m, n))
+        for i, phi in enumerate(self.formulae):
+            for j, w in enumerate(self.worlds):
+                if phi == "iv(fa(exactly_one, player), tv(made, fa(some, shot), self.worlds, player))":
+                    print eval(phi)((0,1,2))
+                if phi and eval(phi)(w):
+                    mat[i,j] = 1.0
+        if self.nullmsg:
+            mat[-1] = np.ones(n)
+        return mat
  
     def get_all_refinements(self):
-        ref = {}
-        for word_and_typ, den in self.baselexicon.items():
-            word, typ = word_and_typ
-            val = []
+        refine = {}
+        for word, semval in self.baselexicon.items():
             if word in self.refinable:
-                if is_intensional_type(typ):
-                    val = [den]
-                else:
-                    val = self.refinements(den, self.refinable[word])
+                refine[word] = self.refinements(semval)
             else:
-                val = [den]
-            ref[(word, typ)] = val
-        return ref
-                                            
-    def characteristic_set(self, func, dom, minsize=0):
-        return [X for X in powerset(dom, minsize=minsize) if func(X)]
+                refine[word] = [semval]
+        return refine
 
-    def refinements(self, semval, dom=None):
-        if isinstance(semval, list):
-            return powerset(func, minsize=1)
-        else:
-            refined = []
-            for X in powerset(self.characteristic_set(semval, dom, minsize=0), minsize=1):
-                refined.append((lambda x, X=X : x in X))
-        return refined
+    def refinements(self, semval):
+        return powerset(semval, minsize=1)
        
 ######################################################################
 
@@ -74,18 +71,21 @@ if __name__ == '__main__':
     players = [a,b]
     shots = [s1,s2]
     basic_states = (0,1)
-        
-    lex = copy(BASELEXICON)
-    lex[("some_player", TYPE_NP)] =  (lambda Y : len(set(players) & set(Y)) > 0)
-    lex[("every_player", TYPE_NP)] = (lambda Y : set(players) <= set(Y))
-    lex[("no_player", TYPE_NP)] =    (lambda Y : len(set(players) & set(Y)) == 0)        
+    worlds = get_worlds(basic_states=(0,1), length=2, increasing=False)
+    baselexicon = define_lexicon(player=players, shot=shots, worlds=worlds)
 
     ug = UncertaintyGrammars(
-        baselexicon=lex,
-        messages=["PlayerA(scored)", "PlayerB(scored)", "every_player(scored)", "no_player(scored)", "some_player(scored)"],
-        worlds=WorldSet(basic_states=basic_states, p=players, s=shots, increasing=False),
-        refinable={'some_player': [a,b], 'intensional_scored': [a,b]},
+        baselexicon=baselexicon,
+        messages=[("PlayerA(scored)",       "iv(PlayerA, scored)"),
+                  ("PlayerB(scored)",       "iv(PlayerB, scored)"),
+                  ("every(player)(scored)", "iv(fa(every, player), scored)"),
+                  ("no(player)(scored)",    "iv(fa(no, player), scored)"),
+                  ("some_player(scored)",   "iv(some_player, scored)")],
+        worlds=worlds,
+        refinable=('scored',), # 'scored'),
         nullmsg=True)
 
+    worldnames = [worldname(w) for w in worlds]
+    print ug.messages
     for lex in ug.lexicon_iterator():
-        display_matrix(lex, rnames=ug.messages, cnames=ug.worlds.names)
+        display_matrix(lex, rnames=ug.messages, cnames=worldnames)
